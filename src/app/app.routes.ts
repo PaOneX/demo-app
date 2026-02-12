4c3923146312daff8c5977a0ce58722b85e0b787
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-02-12 11:50:24 +0530

    feat: implement about and profile pages with routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,27 @@
 import { Routes } from '@angular/router';
 import { Contact } from './page/contact/contact';
+import { Home } from './page/home/home';
+import { About } from './page/about/about';
+import { Profile } from './page/profile/profile';
 
 export const routes: Routes = [
 
     {
         path: 'contact',
         component: Contact
+    },
+    {
+        path:'home',
+        component:Home   
+    },
+    {
+        path:'about',
+        component:About
+    },
+    {
+        path:'profile',
+        component:Profile   
     }
-
     
 
 ];
